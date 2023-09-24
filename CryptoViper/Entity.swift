8f98167482d4898dcacd3f3b7cae104da01ad995
Onur Bulut <10urbulut@gmail.com>
2023-09-24 19:05:23 +0300

    First commit

---
+++ CryptoViper/Entity.swift
@@ -0,0 +1,13 @@
+//
+//  Entity.swift
+//  CryptoViper
+//
+//  Created by Onur Bulut on 24.09.2023.
+//
+
+import Foundation
+
+struct Crypto : Decodable {
+    let currency : String
+    let price : String
+}
